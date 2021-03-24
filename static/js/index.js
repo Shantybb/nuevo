258d53d7c62778ea24af69ed84282de949a4ae3f
Shantybb <81274311+Shantybb@users.noreply.github.com>
2021-03-24 11:01:38 -0500

    Update index.js

--- static/js/index.js
+++ static/js/index.js
@@ -5,7 +5,7 @@ function LED1_On() {
 	console.log("led on");
 	//document.getElementById("sensor").innerHTML="led on";
 	message = new Paho.MQTT.Message("ON");
-    	message.destinationName = "lfrenteriax@hotmail.com/test1";
+    	message.destinationName = "sdarmas.fie@unach.edu.ec/test";
     	client.send(message);
   
 }
@@ -13,7 +13,7 @@ function LED1_Off(){
 	//alert("led off");
 	console.log("led off");
 	message = new Paho.MQTT.Message("OFF");
-    	message.destinationName = "lfrenteriax@hotmail.com/test1";
+    	message.destinationName = "sdarmas.fie@unach.edu.ec/test";
     	client.send(message);
 	//document.getElementById("sensor").innerHTML="led off";
 }
@@ -49,7 +49,7 @@ function LED1_Off(){
 	
     client.subscribe("lfrenteriax@hotmail.com/test");
     message = new Paho.MQTT.Message("hola desde la web");
-    message.destinationName = "lfrenteriax@hotmail.com/test1";
+    message.destinationName = "sdarmas.fie@unach.edu.ec/test";
     client.send(message);
 	
   }
