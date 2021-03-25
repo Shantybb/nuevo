88eabcca81408a822ac88ce0233cb5ec3093ed57
Shantybb <81274311+Shantybb@users.noreply.github.com>
2021-03-24 23:10:18 -0500

    Update index.js

--- static/js/index.js
+++ static/js/index.js
@@ -4,7 +4,7 @@ function LED1_On() {
 	//alert("led on");
 	console.log("led on");
 	//document.getElementById("sensor").innerHTML="led on";
-	message = new Paho.MQTT.Message("ON");
+	message = new Paho.MQTT.Message("LED1_ON");
     	message.destinationName = "sdarmas.fie@unach.edu.ec/test";
     	client.send(message);
   
@@ -12,7 +12,7 @@ function LED1_On() {
 function LED1_Off(){	
 	//alert("led off");
 	console.log("led off");
-	message = new Paho.MQTT.Message("OFF");
+	message = new Paho.MQTT.Message("LED1_OFF");
     	message.destinationName = "sdarmas.fie@unach.edu.ec/test";
     	client.send(message);
 	//document.getElementById("sensor").innerHTML="led off";
